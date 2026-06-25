a559d7aeda5a1c14075a2791023389eaf5acb822
Carlos Eduardo <cesilva29@hotmail.com>
2026-06-25 15:30:04 -0300

    Alteração do exemplo 01 para 02

---
+++ Aula01_bootcamp/exemplo_02.py
@@ -0,0 +1,2 @@
+# Crie um programa que o usuário digite dois números e o programa mostre a soma dos dois números digitados. 
+print(int(input("Digite o primeiro numero: ")) + int(input("Digite o segundo numero: ")))
